00d0f0e08f8ba4122379d09ea21a2b7fe17e48cc
Sky-General <SGeneral109@gmail.com>
2014-04-07 16:33:25 -0700

    Random commit

--- src/com/mwcraft/chat/Main.java
+++ src/com/mwcraft/chat/Main.java
@@ -1,5 +1,13 @@
 package com.mwcraft.chat;
 
-public class Main {
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	public void onEnable(){
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new pluginListener(), this /*I am doing this, because idk if I do the same as listener... */);
+	}
 
 }
---
+++ src/com/mwcraft/chat/pluginListener.java
@@ -0,0 +1,7 @@
+package com.mwcraft.chat;
+
+import org.bukkit.event.Listener;
+
+public class pluginListener implements Listener {
+
+}
